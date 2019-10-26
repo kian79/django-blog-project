370615901c9337167187daea990aa104586b4e1c
kiankr <kiankr79@gmail.com>
2019-10-26 23:26:54 +0330

    added some views and made urls

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -1,6 +1,13 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
+from my_blog import views
 
 urlpatterns = [
-
+    path('/',views.PostListView.as_view(),name="post_list"),
+    path('about/',views.AboutView.as_view(),name='about'),
+    url(r'^post/(?P<pk>\d+)$',views.PostDetailView.as_view(),name="post_detail"),
+    path('post/new/', views.CreatePostView.as_view(), name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$',views.PostUpdateView.as_view(),name='post_edit'),
+    url(r'^post/(?P<pk>\d+)/remove/$',views.PostDeleteView.as_view(),name='post_remove'),
 ]
--- my_blog/views.py
+++ my_blog/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from my_blog.models import *
+from my_blog.forms import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
+
 
 # Create your views here.
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login'
+    redirect_field_name = 'post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'post_list.html'
+    model = Post
