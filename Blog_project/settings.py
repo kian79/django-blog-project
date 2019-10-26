a442014221bcfa8cca94a5dc5063a6c9ff6592fc
kiankr <kiankr79@gmail.com>
2019-10-26 22:24:57 +0330

    forms finished

--- Blog_project/settings.py
+++ Blog_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Blog_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates/blog')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+LOGIN_REDIRECT_URL='/'
